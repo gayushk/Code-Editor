041fd2e2644ba1cf1a1258ee453db9394ed851ea
gayushk <the.gay@bk.ru>
2026-03-22 19:25:58 +0400

    added sockets

---
+++ Network/socket.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+#include <stdexcept>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/netinet/in.h>
+#include <sys/netinet/tcp.h>
+#include <arpa/inet.h>
+
+namespace SimpleNet {
+	
+class Socket {
+	
+public: 
+	int get_fd() const {return fd_;}
+	Socket();
+	~Socket();
+
+	Socket(const Socket&) = delete;
+	Socket& operator=(const Socket&) = delete;
+
+	Socket(Socket&& oth) noexcept;
+	Socket& operator=(Socket&& oth) noexcept;
+
+	void bind(int port);
+	void listen(int backlog = 10);
+	void connect(const std::string& ip, int port);
+
+	Socket accept();
+
+	std::vector<char> receive(size_t max_size = 4096);
+	ssize_t send(std::string_view msg);
+
+private:
+	explicit Socket(int fd) : fd_(fd) {}
+	int fd_ = -1;	
+};	
+
+};
